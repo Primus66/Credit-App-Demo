c4e26e6ee2d03656a7c95fc87908a514d784c684
Primus66 <adetayoadegoroye@gmail.com>
2023-01-04 03:23:04 +0100

    edits

--- router.js
+++ router.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 const  credential = {
-    email : "admin@gmail.com",
-    password : "admin123"
+    email : "LendSqr@gmail.com",
+    password : "1234"
 }
 
 // login user
